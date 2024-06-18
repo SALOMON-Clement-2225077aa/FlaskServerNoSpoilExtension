80149369f85dd8465cc8cf6d89057103f8b4991b
Clément Salomon <114286471+SALOMON-Clement-2225077aa@users.noreply.github.com>
2024-06-18 17:47:16 -0400

    Update flaskMain.py

--- flaskMain.py
+++ flaskMain.py
@@ -17,16 +17,15 @@ def test():
 def get_video_topic():
     blur_tags = ['NN', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ', 'CD','EM']
     video_title = request.args.get('videoTitle')
-    print(video_title)
     words = word_tokenize(video_title)
     tagged_words = pos_tag(words)
     blurred = []
     for word, tag in tagged_words:
-        if tag in blur_tags: blurred.append('[BLUR]')
-        elif len(tag) == 1 : pass
+        if tag in blur_tags: blurred.append('<span class="blur-text">[BLUR]</span>')
+        elif (len(word)==1) or (word=="n't") : pass
         else: blurred.append(word)
     topic = ' '.join(blurred)
-    return jsonify({'videoTitle': video_title, 'topic': topic})
+    return jsonify({'videoTitle': video_title, 'topic': topic, 'tagged_words':tagged_words})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
